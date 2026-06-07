b3a066badc1213b65749e984770cac06ffd5d4b9
Quinn Fransen <qpfransen@gmail.com>
2026-06-06 21:29:15 -0400

    prettier

--- src/app/projects/liquor-store-nav/page.tsx
+++ src/app/projects/liquor-store-nav/page.tsx
@@ -29,7 +29,10 @@ function calculateDistance(
 
   const a =
     Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) +
-    Math.cos(phi1) * Math.cos(phi2) * Math.sin(deltaLambda / 2) * Math.sin(deltaLambda / 2);
+    Math.cos(phi1) *
+      Math.cos(phi2) *
+      Math.sin(deltaLambda / 2) *
+      Math.sin(deltaLambda / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
   return R * c;
